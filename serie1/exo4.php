11304d5d0467279541e96a759991724e9ba6ba24
BEYHAN NASSIM <beyhan_n@JEAN-ROSTAND.lan>
2024-10-10 11:41:09 +0200

    exo4

---
+++ serie1/exo4.php
@@ -0,0 +1,12 @@
+<?php
+
+$argument1 = "Antoine";
+$argument2 = "Griezmann";
+function concatenation($argument1, $argument2) {
+    return $argument1.$argument2;
+}
+
+echo concatenation($argument1,$argument2);
+
+?>
+
