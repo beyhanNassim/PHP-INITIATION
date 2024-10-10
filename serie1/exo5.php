7e8f965a0fd9985c228ecf585e31502bf1d9b53e
BEYHAN NASSIM <beyhan_n@JEAN-ROSTAND.lan>
2024-10-10 11:44:17 +0200

    exo5

---
+++ serie1/exo5.php
@@ -0,0 +1,11 @@
+<?php
+
+$argument1 = "Ngolo";
+$argument2 = "Kante";
+function concatenation($argument1, $argument2) {
+    return $argument1." ".$argument2;
+}
+
+echo concatenation($argument1,$argument2);
+
+?>
